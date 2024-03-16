0e76bc6ae7a439489b3e15e27f0420a6cc351bb3
Marc Hadfield <marc@vital.ai>
2024-03-16 14:39:45 -0400

    sync

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vital-logic',
+    version='0.1.0',
+    author='Marc Hadfield',
+    author_email='marc@vital.ai',
+    description='Vital Logic',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/vital-ai/vital-logic-python',
+    packages=find_packages(),
+    license='Apache License 2.0',
+    install_requires=[
+            'vital-ai-vitalsigns',
+            'six',
+            'pyyaml'
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
